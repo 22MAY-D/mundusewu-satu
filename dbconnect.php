6ec09966d68532b8f3e2f5ff1114ed27267104e9
22MAY-D <65173851+22MAY-D@users.noreply.github.com>
2022-11-01 20:15:36 +0700

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("localhost","root","","mndswst");
+
+if(!$conn){
+	echo "Gagal Menghubungkan ke Database!";
+} else {
+	
+};
+
+?>
